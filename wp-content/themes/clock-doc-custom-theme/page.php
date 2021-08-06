11503be201d6fa7611d83ba0e732bbc327421508
Jim <JimFuhrman@Yahoo.com>
2021-08-06 09:56:40 -0500

    making contact info section and About Us page look normal

--- wp-content/themes/clock-doc-custom-theme/page.php
+++ wp-content/themes/clock-doc-custom-theme/page.php
@@ -4,13 +4,14 @@ while(have_posts()) {
    /* pageBanner(); */ /* This pageBanner function doesn't work. */
    get_template_part('template-parts/content', 'pageBanner')
    ?>
-    <div class="container container--narrow page-section">
+   <!-- I don't want container--narrow in the next div because it makes everything on the About Us page too scrunched together on the largest desktops. -->
+    <div class="container page-section">
     <?php
       $theParent = wp_get_post_parent_id(get_the_ID());
       if ($theParent) { ?>
         <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
-    </div>
+            <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+        </div>
       <?php }
     ?>
  
@@ -20,7 +21,7 @@ while(have_posts()) {
         ));
         if($theParent or $testArray) { ?>
             <div class="page-links">
-                <h2 class="page-links__title"><a href="#"><?php echo get_the_title($theParent);?></a></h2>
+                <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent);?>"><?php echo get_the_title($theParent);?></a></h2>
                 <ul class="min-list">
                     <?php
                         if ($theParent) {
