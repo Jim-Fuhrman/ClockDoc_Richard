e1135a87ea7e9ec817d7a428649e394280f3c678
Jim <JimFuhrman@Yahoo.com>
2021-08-09 14:14:54 -0500

    making footer section look good and uploading more pictures and clock style pages

--- wp-content/themes/clock-doc-custom-theme/footer.php
+++ wp-content/themes/clock-doc-custom-theme/footer.php
@@ -5,13 +5,12 @@
                     <div class="site-footer__col-one">
                         <div class="site-header__nav-icon" style="background-image: url(<?php echo get_theme_file_uri('/images/ClockDocIcon.png');?>"><a href="<?php echo esc_url(site_url('')); ?>"></a>
                         </div>
-                        <h1><a href="<?php echo site_url(); ?>">Clock Doctor</a></h1>
-                        <p>&copy; Copyright 2021 | Clock Doctor | All Rights Reserved</p>
-                        <p>Website created by Jim Fuhrman</p>
+                        <h2><a href="<?php echo site_url(); ?>">Clock Doctor</a></h2>
+                        <p>402-798-7470</p>
                     </div>
                     <div class="site-footer__col-two-three-group">
                         <div class="site-footer__col-two">
-                            <h3 class="headline headline--small">Shop around</h3>
+                            <h3 class="headline headline--medium">Shop around</h3>
                             <nav class="nav-list">
                                 <ul>
                                     <li><a href="<?php echo site_url('') ?>">Home</a></li>
@@ -23,25 +22,28 @@
                             </nav>
                         </div>
                         <div class="site-footer__col-three">
-                            <h3 class="headline headline--small">Learn</h3>
+                            <h3 class="headline headline--medium">Learn</h3>
                             <nav class="nav-list">
                                 <ul>
-                                    <li><a href="#">Legal</a></li>
-                                    <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy</a></li>
-                                    <li><a href="#">Careers</a></li>
+                                    <li><a href="<?php echo site_url('/conditions-of-use');?>">Conditions of Use</a></li>
+                                    <li><a href="<?php echo site_url('/privacy-policy'); ?>">Privacy Policy</a></li>
+                                    <li><a href="<?php echo site_url('/guidelines-for-ratings-and-reviews');?>; ">Guidelines for Reviews</a></li>
                                 </ul>
                             </nav>
                         </div>
                     </div>
                     <div class="site-footer__col-four">
-                        <h3 class="headline headline--small">Connect With Us</h3>
+                        <h3 class="headline headline--medium t-center">Connect With Us</h3>
                         <nav>
                             <ul class="min-list social-icons-list group"> <!-- social-icons-list is in shame.css, min-list is in utility-classes.css -->
                                 <li><a href="https://www.facebook.com/The-Clock-Doctor-159971937541412" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
                                 <li><a href="https://www.linkedin.com/in/richard-bouc-a535a680/" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
                             </ul>
                         </nav>
-                    </div>
+                    </div>    
+                </div>
+                <div class="site-footer__copyright-info">
+                    <p class="t-small">&copy; Copyright 2021 | Clock Doctor | All Rights Reserved | Website created by Jim Fuhrman | Contact <a href="mailto: Jim Fuhrman@Yahoo.com">Jim Fuhrman</a></p>
                 </div>
             </div>
         </footer>
--- wp-content/themes/clock-doc-custom-theme/single-clocks.php
+++ wp-content/themes/clock-doc-custom-theme/single-clocks.php
@@ -5,7 +5,7 @@
 ?>
     <div class="wrapper">
         <h1><?php the_title();?></h1>
-        <p><?php the_content();?></p>
+        <p><?php the_post_thumbnail(); the_content();?></p>
     </div>
 <?php
     get_footer();
