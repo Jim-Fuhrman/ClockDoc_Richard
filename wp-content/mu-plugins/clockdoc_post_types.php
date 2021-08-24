0671a42f2b63375972a248fbeb0ee0d4d3ac939a
Jim <JimFuhrman@Yahoo.com>
2021-08-24 12:40:35 -0500

    adding archive-clocks, archive-watches, archive-misc_items and all refs to them

--- wp-content/mu-plugins/clockdoc_post_types.php
+++ wp-content/mu-plugins/clockdoc_post_types.php
@@ -15,6 +15,36 @@ function register_clockdoc_post_types() {
         ),
         'menu_icon' => 'dashicons-clock'
     ));
+
+    register_post_type('watches', array(
+        'supports' => array('title', 'editor', 'excerpt', 'thumbnail'),
+        'show_in_rest' => true, 
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Watch Styles',
+            'add_new_item' => 'Add New Watch Style', 
+            'edit_item' => 'Edit Watch Style',
+            'all_items' => 'All Watch Styles',
+            'singular_name' => 'Watch Style'
+        ),
+        'menu_icon' => 'dashicons-backup'
+    ));
+
+    register_post_type('misc_items', array(
+        'supports' => array('title', 'editor', 'excerpt', 'thumbnail'),
+        'show_in_rest' => true, 
+        'has_archive' => true,
+        'public' => true,
+        'labels' => array(
+            'name' => 'Misc Items',
+            'add_new_item' => 'Add New Misc Item', 
+            'edit_item' => 'Edit Misc Item',
+            'all_items' => 'All Misc Items',
+            'singular_name' => 'Misc Item'
+        ),
+        'menu_icon' => 'dashicons-bank'
+    ));
 }
 
 add_action('init', 'register_clockdoc_post_types');
