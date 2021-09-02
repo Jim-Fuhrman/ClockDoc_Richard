ce3cc58c5a0286b97828cc1817dcbb146cba1657
Jim <JimFuhrman@Yahoo.com>
2021-09-02 13:20:10 -0500

    getting the CSS grid on smaller screens looking good.

--- wp-content/themes/clock-doc-custom-theme/template-parts/content-products-gallery.php
+++ wp-content/themes/clock-doc-custom-theme/template-parts/content-products-gallery.php
@@ -1,36 +1,65 @@
 <div class="gallery">
-    <figure class="gallery__item gallery__item--1"><img src="<?php echo get_theme_file_uri('/images/shutterstock_JimF_Clock_Kitchen-150x150.jpg'); ?>" alt="Gallery img 1" class="gallery__img"></figure>   
-    <figure class="gallery__item gallery__item--2"><img src="<?php echo get_theme_file_uri('/images/BoucSelf_Three_Clocks-scaled-1-320x150.jpg'); ?>" alt="Gallery img 2" class="gallery__img"></figure>      
-    <figure class="gallery__item gallery__item--3"><img src="<?php echo get_theme_file_uri('/images/shutterstock_JimF_Gold_2020_2021_2022-320x150.jpg'); ?>" alt="Gallery img 3" class="gallery__img"></figure>
-    <figure class="gallery__item gallery__item--4"><img src="<?php echo get_theme_file_uri('/images/Bouc_Watch_Time_Temp-150x150.jpg'); ?>" alt="Gallery img 4" class="gallery__img"></figure>
-    <figure class="gallery__item gallery__item--5">   
-        <img src="<?php echo get_theme_file_uri('/images/BoucSelf_Clock_Maroon_Tamber-320x320.jpeg'); ?>" alt="Gallery img 5" class="gallery__img">
-    </figure>
-    <figure class="gallery__item gallery__item--6">
-        <picture>
-            <source srcset="<?php echo get_theme_file_uri('/images/Bouc_Grandfather_ClockFace-150x320.jpg'); ?>" media="(max-width: 33.125em)">
-            <img src="<?php echo get_theme_file_uri('/images/BoucSelf_Clock_SixPillars-320x320.jpeg'); ?>" alt="Gallery img 6" class="gallery__img">
-        </picture>
-    </figure>        
-    
-    <figure class="gallery__item gallery__item--7">
-        <picture>
-            <source srcset="<?php echo get_theme_file_uri('/images/BoucSelf_Clock_SixPillars-320x320.jpeg'); ?>" media="(max-width: 33.125em)">
-            <img src="<?php echo get_theme_file_uri('/images/Bouc_Grandfather_ClockFace-150x320.jpg'); ?>" alt="Gallery img 7" class="gallery__img">
-        </picture>    
-    </figure>
-    <figure class="gallery__item gallery__item--8"><img src="<?php echo get_theme_file_uri('/images/BoucSelf_Clock_Black_Face_Glass_Cover-150x320.jpeg'); ?>" alt="Gallery img 8" class="gallery__img"></figure>
-    <figure class="gallery__item gallery__item--9"><img src="<?php echo get_theme_file_uri('/images/shutterstock_Jimf_Gold_WatchBand_BlackFace-320x150.jpg'); ?>" alt="Gallery img 9" class="gallery__img"></figure>
-    <figure class="gallery__item gallery__item--10">
-        <source srcset="<?php echo get_theme_file_uri('/images/shutterstock_Clock_Ancient_clear-1024x591-1-490x150.jpg'); ?>" media="(max-width: 33.125em)">
-        <img src="<?php echo get_theme_file_uri('/images/shutterstock_Clockwork_Colorful_Jims_Pick-320x150.jpg'); ?>" alt="Gallery img 10" class="gallery__img">
-    </figure>
-    <figure class="gallery__item gallery__item--11">
-        <source srcset="<?php echo get_theme_file_uri('/images/shutterstock_JimF_Watch_Bracelet_Gold_small-150x150.jpg'); ?>" media="(max-width: 33.125em)">
-        <img src="<?php echo get_theme_file_uri('/images/shutterstock_JimF_Watch_Bracelet_Gold_small-320x150.jpg'); ?>" alt="Gallery img 11" class="gallery__img">
-    </figure>
-    <figure class="gallery__item gallery__item--12"><img src="<?php echo get_theme_file_uri('/images/Bouc_Watches_Pocket-150x150.jpg'); ?>" alt="Gallery img 12" class="gallery__img"></figure>
-    <figure class="gallery__item gallery__item--13"><img src="<?php echo get_theme_file_uri('/images/shutterstock_JimF_Flat_Watch_SilverBand-320x150.jpg'); ?>" alt="Gallery img 13" class="gallery__img"></figure>
-    <figure class="gallery__item gallery__item--14"><img src="<?php echo get_theme_file_uri('/images/shutterstock_Clock_Ancient_clear-1024x591-1-320x150.jpg'); ?>" alt="Gallery img 14" class="gallery__img"></figure>
-    <figure class="gallery__item gallery__item--15"><img src="<?php echo get_theme_file_uri('/images/Bouc_Watch_and_picker-150x150.jpg'); ?>" alt="Gallery img 15" class="gallery__img"></figure>
-</div>
+        <figure class="gallery__item gallery__item--1"><img src="<?php echo get_theme_file_uri('/images/shutterstock_JimF_Clock_Kitchen-150x150.jpg'); ?>" alt="Gallery img 1" class="gallery__img"></figure>   
+        <figure class="gallery__item gallery__item--2"><img src="<?php echo get_theme_file_uri('/images/BoucSelf_Three_Clocks-scaled-1-320x150.jpg'); ?>" alt="Gallery img 2" class="gallery__img"></figure>      
+        <figure class="gallery__item gallery__item--3"><img src="<?php echo get_theme_file_uri('/images/shutterstock_JimF_Gold_2020_2021_2022-320x150.jpg'); ?>" alt="Gallery img 3" class="gallery__img"></figure>
+        <figure class="gallery__item gallery__item--4"><img src="<?php echo get_theme_file_uri('/images/Bouc_Watch_Time_Temp-150x150.jpg'); ?>" alt="Gallery img 4" class="gallery__img"></figure>
+        <figure class="gallery__item gallery__item--5">   
+            <img src="<?php echo get_theme_file_uri('/images/BoucSelf_Clock_Maroon_Tamber-320x320.jpeg'); ?>" alt="Gallery img 5" class="gallery__img">
+        </figure>
+        <figure class="gallery__item gallery__item--6">
+            <picture>
+                <source srcset="<?php echo get_theme_file_uri('/images/Bouc_Grandfather_ClockFace-150x320.jpg'); ?>" media="(max-width: 75em)">
+                <img src="<?php echo get_theme_file_uri('/images/BoucSelf_Clock_SixPillars-320x320.jpeg'); ?>" alt="Gallery img 6" class="gallery__img">
+            </picture>
+        </figure>        
+        
+        <figure class="gallery__item gallery__item--7">
+            <picture>
+                <source srcset="<?php echo get_theme_file_uri('/images/BoucSelf_Clock_Black_Face_Glass_Cover-150x320.jpeg'); ?>" media="(max-width: 75em)">
+                <img src="<?php echo get_theme_file_uri('/images/Bouc_Grandfather_ClockFace-150x320.jpg'); ?>" alt="Gallery img 7" class="gallery__img">
+            </picture>    
+        </figure>
+        <figure class="gallery__item gallery__item--8">
+            <picture>
+                <source srcset="<?php echo get_theme_file_uri('/images/BoucSelf_Clock_SixPillars-320x320.jpeg'); ?>" media="(max-width: 75em">
+                <img src="<?php echo get_theme_file_uri('/images/BoucSelf_Clock_Black_Face_Glass_Cover-150x320.jpeg'); ?>" alt="Gallery img 8" class="gallery__img">
+            </picture>
+        </figure>
+        <figure class="gallery__item gallery__item--9"><img src="<?php echo get_theme_file_uri('/images/shutterstock_JimF_Watch_Bracelet_Gold_small-320x150.jpg'); ?>" alt="Gallery img 9" class="gallery__img"></figure>
+        <figure class="gallery__item gallery__item--10">
+            <picture>
+                <source srcset="<?php echo get_theme_file_uri('/images/Bouc_Watches_Pocket-150x150.jpg'); ?>" media="(max-width: 75em)">
+                <img src="<?php echo get_theme_file_uri('/images/shutterstock_Clockwork_Colorful_Jims_Pick-320x150.jpg'); ?>" alt="Gallery img 10" class="gallery__img">
+            </picture>
+        </figure>
+        <figure class="gallery__item gallery__item--11">
+            <picture>            
+                <source srcset="<?php echo get_theme_file_uri('/images/Bouc_Watch_and_picker-150x150.jpg'); ?>" media="(max-width: 75em)">
+                <img src="<?php echo get_theme_file_uri('/images/shutterstock_JimF_Flat_Watch_SilverBand-320x150.jpg'); ?>" alt="Gallery img 11" class="gallery__img">
+            </picture>
+        </figure>
+        <figure class="gallery__item gallery__item--12">
+            <picture>
+                <source srcset="<?php echo get_theme_file_uri('/images/shutterstock_Clockwork_Colorful_Jims_Pick-320x150.jpg'); ?>" media="(max-width: 75em)">
+                <img src="<?php echo get_theme_file_uri('/images/Bouc_Watches_Pocket-150x150.jpg'); ?>" alt="Gallery img 12" class="gallery__img">
+            </picture>
+        </figure>
+        <figure class="gallery__item gallery__item--13">
+            <picture>
+                <source srcset="<?php echo get_theme_file_uri('/images/shutterstock_Jimf_Gold_WatchBand_BlackFace-320x150.jpg'); ?>" media="(max-width: 75em)">
+                <img src="<?php echo get_theme_file_uri('/images/shutterstock_Jimf_Gold_WatchBand_BlackFace-320x150.jpg'); ?>" alt="Gallery img 13" class="gallery__img">
+            </picture>
+        </figure>
+        <figure class="gallery__item gallery__item--14">
+            <picture>
+                <source srcset="<?php echo get_theme_file_uri('/images/shutterstock_Clock_Ancient-150x150.jpg'); ?>" media="(max-width: 75em)">
+                <img src="<?php echo get_theme_file_uri('/images/shutterstock_Clock_Ancient_clear-1024x591-1-320x150.jpg'); ?>" alt="Gallery img 14" class="gallery__img">
+            </picture>
+        </figure>
+        <figure class="gallery__item gallery__item--15">
+            <picture>
+                <source srcset="<?php echo get_theme_file_uri('/images/shutterstock_JimF_Flat_Watch_SilverBand-490x150.jpg'); ?>" media="(max-width: 75em)">
+                <img src="<?php echo get_theme_file_uri('/images/Bouc_Watch_and_picker-150x150.jpg'); ?>" alt="Gallery img 15" class="gallery__img">
+            </picture>
+        </figure>
+    </div>
