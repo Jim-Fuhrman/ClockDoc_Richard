e9804be0f8109ba64699747cef7cbd435c071bf0
Jim <JimFuhrman@Yahoo.com>
2021-09-30 11:13:54 -0500

    removing unnecessary comments from functions.php, coding padding-bottom in site-header

--- wp-content/themes/clock-doc-custom-theme/functions.php
+++ wp-content/themes/clock-doc-custom-theme/functions.php
@@ -1,14 +1,4 @@
 <?php
-/* I don't want to see the black admin bar when I'm signed in because it overlaps the nav bar. 
-I want the nav bar coded position: fixed. That means the black admin bar will overlap it. If
-nav bar is coded anything other than position: fixed, the black admin bar pushes it down 32px. 
-If I code the nav bar "top: 32px", I see an undesired top margin when I'm not signed in and
-I start scrolling down. I don't want that. 
-
-The simplest solution is don't show the black admin bar - ever. */
-
-/*show_admin_bar(false);*/
-
 require get_theme_file_path('/inc/search-route.php');
 require get_theme_file_path('/inc/page-banner.php');
 require get_theme_file_path('/inc/load-clockdoc-files.php');
